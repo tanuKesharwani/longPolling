58afb2d41a71d26fcfab4e4ce256102214abd360
rsk-tanu <tanu.kesharwani@gmail.com>
2025-12-31 21:42:05 +0530

    implemented longPolling

---
+++ src/main/java/com/longPolling/longPolling/LongPollingApplication.java
@@ -0,0 +1,13 @@
+package com.longPolling.longPolling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LongPollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LongPollingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/longPolling/longPolling/LongPollingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.longPolling.longPolling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LongPollingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
